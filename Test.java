128287a546e6c5c3a304c67aa6e250964e3189f9
AkihiroTanaka3 <126536571+AkihiroTanaka3@users.noreply.github.com>
2023-12-09 20:58:13 +0900

    テスト

---
+++ Test.java
@@ -0,0 +1,11 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println(10);
+        System.out.println(-20);
+        System.out.println(2.5);
+        System.out.println(1_000_000);
+        System.out.println(1.2e3);
+        System.out.println(1.2e-3);
+    }
+}
