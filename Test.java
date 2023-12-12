b812c1f21f11694b208ba43d0b7cc683179906ac
AkihiroTanaka3 <126536571+AkihiroTanaka3@users.noreply.github.com>
2023-12-13 00:54:27 +0900

    変更

--- Test.java
+++ Test.java
@@ -1,11 +1,16 @@
 public class Test {
     public static void main(String[] args) {
+        /*
+         Javaにおける数値の表現
+         */
         System.out.println("Hello World");
         System.out.println(10);
         System.out.println(-20);
         System.out.println(2.5);
         System.out.println(1_000_000);
-        System.out.println(1.2e3);
-        System.out.println(1.2e-3);
+        // eを使った指数表記
+        System.out.println(1.2e3); //1.2 * 10^3 (1.2かける10の3乗)
+        System.out.println(1.2e+3); 
+        System.out.println(1.2e-3); //1.2 * 10^-3 (1.2かける10の-3乗)
     }
 }
